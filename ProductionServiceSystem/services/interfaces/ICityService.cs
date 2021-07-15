d8d153e5864bbca9259b820d3644bd40db7b4fa8
Jarek <jaroslawmal11@gmail.com>
2021-07-15 22:19:50 +0200

    Added folder sercices-> interfaces with interfaces: ICalculatorCostService.cs, ICityService.cs, IModuleService.cs, ISearchHistoryService.cs, IShowResultService.cs.

---
+++ ProductionServiceSystem/services/interfaces/ICityService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProductionServiceSystem.Models;
+using ProductionServiceSystem.ModelsDTO;
+
+namespace ProductionServiceSystem.services.interfaces
+{
+    interface ICityService
+    {
+        City GetCityByName(string cityName);
+        OperationSuccesDTO<IList<City>> GetCities();
+        OperationResultDTO UpdateCostOfWorkingHour(string cityName, double workingHourCost);
+        OperationResultDTO UpdateTransportCost(string cityName, double transportCost);
+        OperationResultDTO AddCity(City city);
+        OperationResultDTO DeleteCity(string cityName);
+
+    }
+}
---
+++ ProductionServiceSystem/services/interfaces/IShowResultService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProductionServiceSystem.ModelsDTO;
+
+namespace ProductionServiceSystem.services.interfaces
+{
+    interface IShowResultService
+    {
+        ResultCostDTO PresentResult(string cityName, ModuleListDTO moduleListDTO);
+    }
+}
