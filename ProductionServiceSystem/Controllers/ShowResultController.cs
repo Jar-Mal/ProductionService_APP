ab030b4d2e60d0e1478f31fa65649a7332b5b2a8
jaroslaw-mal <jaroslawmal11@gmail.com>
2021-08-02 23:49:37 +0200

    Added implementation of services, Controllers and DTO

---
+++ ProductionServiceSystem/Controllers/ShowResultController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProductionServiceSystem.services.interfaces;
+using ProductionServiceSystem.Models;
+
+namespace ProductionServiceSystem.Controllers
+{
+    public class ShowResultController : Controller
+    {
+        private readonly IShowResultService _showResultService;
+        public ShowResultController(IShowResultService showResultService)
+        {
+            _showResultService = showResultService;
+        }
+
+        //[HttpPost]
+        //[Route("ShowResult/Get")]
+        //public IActionResult GetCost(ShowResultDTO showResultDTO)
+    }
+
+}
